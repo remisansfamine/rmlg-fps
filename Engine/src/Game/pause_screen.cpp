79fa885973bcf3479867c79fb079d15ace159a14
r.giner <remi.giner@hotmail.fr>
2021-06-03 11:44:30 +0200

    [unstable] - Removing pause screen for the merge

--- Engine/src/Game/pause_screen.cpp
+++ Engine/src/Game/pause_screen.cpp
@@ -19,20 +19,17 @@ namespace Gameplay
 
 	void PauseScreen::start()
 	{
-		Core::Engine::Graph::setCursorState(true);
-	}
-	
-	void PauseScreen::update()
-	{
+		/*Core::Engine::Graph::setCursorState(true);
+
 		Engine::GameObject* goButtonResumeGame = Core::Engine::Graph::findGameObjectWithName("ResumeGameButton");
 		std::shared_ptr<UI::Button> buttonResumeGame = goButtonResumeGame->getComponent<UI::Button>();
 		UI::Button* resumeGameptr = buttonResumeGame.get();
 		resumeGameptr->addListener(UI::ButtonState::DOWN, []() {
 			Core::TimeManager::setTimeScale(1.f);
-			});
+		});
 		resumeGameptr->addListener(UI::ButtonState::HIGHLIGHT, [resumeGameptr]() {
 			resumeGameptr->getSprite()->m_color = Core::Maths::vec4(0.8f, 0.3f, 0.3f, 1.f);
-			});
+		});
 
 
 		Engine::GameObject* goButtonMainMenu = Core::Engine::Graph::findGameObjectWithName("MainMenuButton");
@@ -40,10 +37,10 @@ namespace Gameplay
 		UI::Button* mainMenuptr = buttonMainMenu.get();
 		mainMenuptr->addListener(UI::ButtonState::DOWN, []() {
 			Core::Engine::Graph::loadMainMenu();
-			});
+		});
 		mainMenuptr->addListener(UI::ButtonState::HIGHLIGHT, [mainMenuptr]() {
 			mainMenuptr->getSprite()->m_color = Core::Maths::vec4(0.8f, 0.3f, 0.3f, 1.f);
-			});
+		});
 
 
 		Engine::GameObject* goButtonSaveScene = Core::Engine::Graph::findGameObjectWithName("SaveSceneButton");
@@ -52,10 +49,10 @@ namespace Gameplay
 		saveSceneptr->addListener(UI::ButtonState::DOWN, []() {
 			Core::Engine::Graph::saveCurrentScene();
 			Core::Debug::Log::info("Scene has been saved");
-			});
+		});
 		saveSceneptr->addListener(UI::ButtonState::HIGHLIGHT, [saveSceneptr]() {
 			saveSceneptr->getSprite()->m_color = Core::Maths::vec4(0.8f, 0.3f, 0.3f, 1.f);
-			});
+		});
 
 
 		Engine::GameObject* goButtonExit = Core::Engine::Graph::findGameObjectWithName("ExitButton");
@@ -63,10 +60,15 @@ namespace Gameplay
 		UI::Button* exitPtr = buttonExit.get();
 		exitPtr->addListener(UI::ButtonState::DOWN, []() {
 			Core::Application::closeApplication();
-			});
+		});
 		exitPtr->addListener(UI::ButtonState::HIGHLIGHT, [exitPtr]() {
 			exitPtr->getSprite()->m_color = Core::Maths::vec4(0.8f, 0.3f, 0.3f, 1.f);
-			});
+		});*/
+	}
+	
+	void PauseScreen::update()
+	{
+
 	}
 
 	void PauseScreen::drawImGui()

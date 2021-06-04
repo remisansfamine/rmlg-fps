89de24c88e6d801ad62c18ba4f77befd5b07c697
r.giner <remi.giner@hotmail.fr>
2021-06-04 10:02:03 +0200

    [player_life] - Adding player damages from the enemies

--- Engine/include/Game/Gameplay/Enemy/enemy_state.hpp
+++ Engine/include/Game/Gameplay/Enemy/enemy_state.hpp
@@ -4,13 +4,25 @@
 
 #include <memory>
 
+#include "timer.hpp"
+#include "rigidbody.hpp"
+
 namespace Gameplay
 {
+	class PlayerLife;
+
 	class EnemyState : public EntityState
 	{
+	private:
+		Core::Timer attackCooldown;
+		std::shared_ptr<PlayerLife> playerLife;
+		std::shared_ptr<Physics::Rigidbody> rb;
+
 	public:
 		EnemyState(Engine::GameObject& gameObject);
 
+		void start() override;
+		void update() override;
 		void drawImGui() override;
 
 		std::string toString() const override;
@@ -18,6 +30,6 @@ namespace Gameplay
 		static void parseComponent(Engine::GameObject& gameObject, std::istringstream& iss);
 
 		void onCollisionEnter(const Physics::Collision& collision) override;
-		void onCollisionExit(const Physics::Collision& collision) override;
+		void onCollisionStay(const Physics::Collision& collision) override;
 	};
 }
